ed594783dd62b6098ca395847a36f5c50d81e697
frank2 <frank2@dc949.org>
2022-10-20 05:17:44 -0700

    create mainpage in documentation

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,3 +1,10 @@
+//! @mainpage Yet Another PE Parser!
+//!
+//! Welcome to **YAPP**! This project is the product of multiple successful attempts at parsing
+//! PE executables. It is intended to make the process of accessing PE headers not only easier
+//! on Windows, but accessible on non-Windows platforms!
+//!
+
 #include <yapp.hpp>
 
 #if defined(YAPP_WIN32) && defined(YAPP_SHARED)
